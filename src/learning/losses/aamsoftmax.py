0fdac7a5290c523002f6c53bb525d529cac079a7
hlongng2002241 <nguyenhailongng@gmail.com>
2022-12-24 16:30:27 +0700

    losses

---
+++ src/learning/losses/aamsoftmax.py
@@ -0,0 +1,58 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+from learning.metrics import accuracy
+
+
+class AAMSoftmax(nn.Module):
+    def __init__(self, n_out, n_classes, margin=0.2, scale=30, easy_margin=False):
+        super(AAMSoftmax, self).__init__()
+
+        self.test_normalize = True
+        
+        self.m = margin
+        self.s = scale
+        self.in_feats = n_out
+        self.w = torch.nn.Parameter(torch.FloatTensor(n_classes, n_out), requires_grad=True)
+        self.ce = nn.CrossEntropyLoss()
+        nn.init.xavier_normal_(self.w, gain=1)
+
+        self.easy_margin = easy_margin
+        self.cos_m = math.cos(self.m)
+        self.sin_m = math.sin(self.m)
+
+        # make the function cos(theta+m) monotonic decreasing while theta in [0°,180°]
+        self.th = math.cos(math.pi - self.m)
+        self.mm = math.sin(math.pi - self.m) * self.m
+
+        print('Initialised AAMSoftmax margin %.3f scale %.3f'%(self.m,self.s))
+
+    def forward(self, x, label=None):
+
+        assert x.size()[0] == label.size()[0]
+        assert x.size()[1] == self.in_feats
+        
+        # cos(theta)
+        cosine = F.linear(F.normalize(x), F.normalize(self.w))
+        # cos(theta + m)
+        sine = torch.sqrt((1.0 - torch.mul(cosine, cosine)).clamp(0, 1))
+        phi = cosine * self.cos_m - sine * self.sin_m
+
+        if self.easy_margin:
+            phi = torch.where(cosine > 0, phi, cosine)
+        else:
+            phi = torch.where((cosine - self.th) > 0, phi, cosine - self.mm)
+
+        one_hot = torch.zeros(cosine.size(), device='cuda' if torch.cuda.is_available() else 'cpu')
+        one_hot.scatter_(1, label.view(-1, 1), 1)
+        output = (one_hot * phi) + ((1.0 - one_hot) * cosine)
+        output = output * self.s
+
+        loss    = self.ce(output, label)
+        prec1   = accuracy(output.detach(), label.detach(), topk=(1,))[0]
+        return loss, prec1
+
+
+def loss_init(n_out, n_classes, margin=0.2, scale=30, easy_margin=False, **kwargs):
+    return AAMSoftmax(n_out=n_out, n_classes=n_classes, margin=margin, scale=scale, easy_margin=easy_margin)
---
+++ src/learning/losses/softmaxproto.py
@@ -0,0 +1,36 @@
+import torch.nn as nn
+from .softmax import SoftmaxLoss
+from .angleproto import AngleProto
+
+
+class SoftmaxProto(nn.Module):
+    """
+    Softmaxproto combines the functionality 
+    of two other loss functions: 
+    softmax.SoftmaxProto and angleproto.SoftmaxProto.
+    """
+
+    def __init__(self, n_out: int, n_classes: int, init_w=10.0, init_b=-5.0):
+        super(SoftmaxProto, self).__init__()
+
+        self.test_normalize = True
+
+        self.softmax = SoftmaxLoss(n_out=n_out, n_classes=n_classes)
+        self.angleproto = AngleProto(init_w=init_w,  init_b=init_b)
+
+        print('Initialised SoftmaxPrototypical Loss')
+
+    def forward(self, x, label=None):
+        # The input tensor x is expected to have shape 
+        # (batch_size, 2, feature_dim)
+        assert x.size()[1] == 2
+
+        nlossS, prec1   = self.softmax(x.reshape(-1,x.size()[-1]), label.repeat_interleave(2))
+
+        nlossP, _       = self.angleproto(x,None)
+
+        return nlossS+nlossP, prec1
+
+
+def loss_init(n_out: int, n_classes: int, init_w=10.0, init_b=-5.0, **kwargs):
+    return SoftmaxProto(n_out=n_out, n_classes=n_classes, init_w=init_w, init_b=init_b)
