ad10a81a83f58b7e1f5dc65d8716e7ac54b32938
hlongng2002241 <nguyenhailongng@gmail.com>
2022-12-24 16:31:04 +0700

    add optims and schedulers

---
+++ src/learning/optimizers/adam.py
@@ -0,0 +1,19 @@
+from torch.optim import Adam
+
+
+def optimizer_init(params, lr: float = 1e-3, weight_decay: float = 2e-5, **kwargs) -> Adam:
+    """
+    Adam optimizer based on Pytorch.
+
+    Args
+    ----
+        params (iterable): iterable of parameters to optimize or dicts defining parameter groups.
+        lr (float, optional): learning rate. Default: 1e-3.
+        weight_decay (float, optional): weight decay (L2 penalty). Default: 2e-5.
+
+    Return
+    ------
+        optimizer: torch.optim.Adam
+    """
+    print("Initialised Adam optimizer")
+    return Adam(params = params, lr = lr, weight_decay = weight_decay)
